5003cb19d0ca3ca3898744be69b50acc948dd62b
viniciusvga46@gmail.com <viniciusvga46@gmail.com>
2020-05-14 15:45:36 -0300

    modificado

--- header.php
+++ header.php
@@ -1,25 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta http-equiv=”Content-Type” content=”text/html; charset=utf-8″>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Culturas</title>
+  <meta http-equiv=”Content-Type” content=”text/html; charset=utf-8″>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Culturas</title>
 
-    <link href="css/edit.css" rel="stylesheet" id="edit">
+  <link href="css/edit.css" rel="stylesheet" id="edit">
 
-    <link
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css"
-          rel="stylesheet"
-          id="bootstrap-css">
-    <script
-          src="https://code.jquery.com/jquery-3.4.1.js"
-          integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-          crossorigin="anonymous">
-    </script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous">
+  </script>
 </head>
-<body>
 
-<nav class="navbar navbar-default sidebar" role="navigation">
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Logo</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+         
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Projetos</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <nav class="navbar navbar-default sidebar" role="navigation">
     <div class="container-fluid">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
@@ -35,7 +54,6 @@
           <li class="active"><a href="index.php">Home<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
           <li><a href="cadastrar.php">Cadastro<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a></li>
           <li><a href="listar.php">Listar<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-file"></span></a></li>
-          <li><a href="login.php">Login<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-bell"></span></a></li>
         </ul>
       </div>
     </div>
